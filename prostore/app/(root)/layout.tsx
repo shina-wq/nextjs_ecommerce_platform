bc92568bb68063c1abc1dc34389270d1cf643431
Shina <m.atete@alustudent.com>
2025-09-22 11:20:24 +0200

    Initial Commit

---
+++ prostore/app/(root)/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Prostore",
+  description: "A modern e-commerce platform built with Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-screen flex-col">
+      <main className="flex-1 wrapper">{children}</main>
+    </div>
+  );
+}
